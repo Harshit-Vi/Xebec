959aca058127ecfa8ae0b688327abfd37025ae63
Harshit Singh <harshit.24mim10257@vitbhopal.ac.in>
2025-06-12 23:34:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 # todo: find an alternative to use api key
-apikey = "sk-4d8938d9b3922e0eb7bcf3e46cc0328cb40f58c7cfca46f0aa842a8f6ce86ba4"
+apikey = "sk-tgp_v1_RSVq1hWYbQbyGMussZ57xgLB5yZ_ArpuaHW-TnG36Hk"
